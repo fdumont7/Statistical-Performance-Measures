f01e71f4b7d404a4a2dc2d4c295c972927728bf1
chocobochic7 <chocobochic7@gmail.com>
2020-08-24 15:11:00 -0400

    Cleaned up code

--- StatisticalPerformanceMeasures.py
+++ StatisticalPerformanceMeasures.py
@@ -1,10 +1,12 @@
-import xlrd #whats used to read excel document. get by 'pip install xlrd'
+#Author: Faryn Dumont
+#Formulas from 'Hydrologic and Water Quality Models: Performance Measures and Evaluation Criteria' by D. N. Moriasi, M. W. Gitau, N. Pai, P. Daggupati
+
+import xlrd #whats used to read excel document.
 import math
-#currently the target station is the last item in the document.
+
 class StatisticalPerformanceMeasures:
 
 	def __init__(self):
-		
 		while True:
 			try:
 				myFile = input("What file would you like to open (please put in full location)\n")
@@ -15,14 +17,11 @@ class StatisticalPerformanceMeasures:
 				print("invalid file name please try again")
 		#on_demand is used to avoid loading worksheet data into memory
 		self.workBook = xlrd.open_workbook(myFile, on_demand = True)
-		
-		
-
+	
 	def getObservedOutflow(self):
 		OO = []
 		for i in range(len(self.workBook.sheet_names())):
 			OO.extend(self.workBook.sheet_by_index(i).col_values(1)[1:])
-
 		while('' in OO):
 			OO.remove('')
 		return OO
@@ -31,10 +30,8 @@ class StatisticalPerformanceMeasures:
 		SO = []
 		for i in range(len(self.workBook.sheet_names())):
 			SO.extend(self.workBook.sheet_by_index(i).col_values(4)[1:])
-
 		while('' in SO):
 			SO.remove('')
-
 		return SO
 
 	def calculateNSE(self):
@@ -46,7 +43,6 @@ class StatisticalPerformanceMeasures:
 		for i in range(len(OO)):
 			top =top + (OO[i]-SO[i])**2
 			bottom = bottom + (OO[i]-meanOO)**2
-
 		return 1-top/bottom
 
 	def calculatePBIAS(self):
@@ -56,7 +52,6 @@ class StatisticalPerformanceMeasures:
 		bottom = sum(OO)
 		for i in range(len(OO)):
 			top = top + (OO[i]-SO[i])
-
 		return (top/bottom)*100
 
 	def calculateRSR(self):
@@ -79,7 +74,6 @@ class StatisticalPerformanceMeasures:
 		for i in range(len(OO)):
 			top =top + (OO[i]-SO[i])**2
 			bottom = bottom + (abs(SO[i]-meanOO)+abs(OO[i]-meanOO))**2
-
 		return 1-top/bottom
 			
 
