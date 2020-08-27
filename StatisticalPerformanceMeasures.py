dcf486a39292cde45ffb78c250fe964e7ec3cc4d
chocobochic7 <chocobochic7@gmail.com>
2020-08-27 11:32:52 -0400

    added more calculations

--- StatisticalPerformanceMeasures.py
+++ StatisticalPerformanceMeasures.py
@@ -29,7 +29,7 @@ class StatisticalPerformanceMeasures:
 	#returns 1d list with the list containing all data from all sheets
 	def getObservedOutflowAll(self):
 		OO = []
-		for i in range(len(self.workBook.sheet_names())):
+		for i in range(self.workBook.nsheets):
 			if self.workBook.sheet_by_index(i).visibility == 0:
 				OO.extend(self.workBook.sheet_by_index(i).col_values(1)[1:])
 		while('' in OO):
@@ -39,7 +39,7 @@ class StatisticalPerformanceMeasures:
 	#returns 1d list with the list containing all data from all sheets 
 	def getSimulatedOutflowAll(self):
 		SO = []
-		for i in range(len(self.workBook.sheet_names())):
+		for i in range(self.workBook.nsheets):
 			if self.workBook.sheet_by_index(i).visibility == 0:
 				SO.extend(self.workBook.sheet_by_index(i).col_values(4)[1:])
 		while('' in SO):
@@ -49,7 +49,7 @@ class StatisticalPerformanceMeasures:
 	#returns 2d list with the list containing individual lists for each sheet
 	def getObservedOutflowIndividual(self):
 		OO = []
-		for i in range(len(self.workBook.sheet_names())):
+		for i in range(self.workBook.nsheets):
 			if self.workBook.sheet_by_index(i).visibility == 0:
 				OO.append(self.workBook.sheet_by_index(i).col_values(1)[1:])
 				while ('' in OO[i]):
@@ -59,13 +59,30 @@ class StatisticalPerformanceMeasures:
 	# returns 2d list with the list containing individual lists for each sheet
 	def getSimulatedOutflowIndividual(self):
 		SO = []
-		for i in range(len(self.workBook.sheet_names())):
+		for i in range(self.workBook.nsheets):
 			if self.workBook.sheet_by_index(i).visibility == 0:
 				SO.append(self.workBook.sheet_by_index(i).col_values(4)[1:])
 			while ('' in SO):
 				SO.remove('')
 		return SO
 
+	def calculate_r(self, observedOutflow, simulatedOutflow):
+		OO = observedOutflow
+		SO = simulatedOutflow
+		meanOO = sum(OO)/len(OO)
+		meanSO = sum(OO)/len(OO)
+		top = 0.0
+		bottomLeft = 0.0
+		bottomRight = 0.0
+		for i in range(len(OO)):
+			top = top + ((OO[i] - meanOO)*(SO[i] - meanSO))
+			bottomLeft = bottomLeft + (OO[i] - meanOO)**2
+			bottomRight = bottomRight + (SO[i] - meanSO)**2
+		return top/(math.sqrt(bottomLeft)*math.sqrt(bottomRight))
+
+	def calculateR2(self, observedOutflow, simulatedOutflow):
+		return self.calculate_r(observedOutflow, simulatedOutflow)**2
+		
 	def calculateNSE(self, observedOutflow, simulatedOutflow):
 		OO = observedOutflow
 		SO = simulatedOutflow
@@ -77,14 +94,36 @@ class StatisticalPerformanceMeasures:
 			bottom = bottom + (OO[i]-meanOO)**2
 		return 1-top/bottom
 
-	def calculatePBIAS(self,observedOutflow, simulatedOutflow):
+	def calculate_d(self, observedOutflow, simulatedOutflow):
 		OO = observedOutflow
 		SO = simulatedOutflow
+		meanOO = sum(OO)/len(OO)
 		top = 0.0
-		bottom = sum(OO)
+		bottom = 0.0
 		for i in range(len(OO)):
-			top = top + (OO[i]-SO[i])
-		return (top/bottom)*100
+			top =top + (OO[i]-SO[i])**2
+			bottom = bottom + (abs(SO[i]-meanOO)+abs(OO[i]-meanOO))**2
+		return 1-top/bottom
+
+	def calculateRMSE(self, observedOutflow, simulatedOutflow):
+		OO = observedOutflow
+		SO = simulatedOutflow
+		num = 0.0
+		for i in range(len(OO)):
+			num = num + (OO[i]-SO[i])**2
+		return math.sqrt((1/len(OO))*num)
+
+	def calculateMAE(self, observedOutflow, simulatedOutflow):
+		OO = observedOutflow
+		SO = simulatedOutflow
+		num = 0.0
+		for i in range(len(OO)):
+			num = num + abs(OO[i]-SO[i])
+		return (1/len(OO))*num
+
+	#can't figure out what equation means. it has Oi and Pi but no sum, or definition of i
+	def calculateRE(self, observedOutflow, simulatedOutflow):
+		return "not calculated"
 
 	def calculateRSR(self, observedOutflow, simulatedOutflow):
 		OO = observedOutflow
@@ -97,15 +136,29 @@ class StatisticalPerformanceMeasures:
 			bottom = bottom + (OO[i]-meanSO)**2
 		return math.sqrt(top)/math.sqrt(bottom)
 
-	def calculate_d(self, observedOutflow, simulatedOutflow):
+	def calculatePBIAS(self,observedOutflow, simulatedOutflow):
 		OO = observedOutflow
 		SO = simulatedOutflow
-		meanOO = sum(OO)/len(OO)
+		top = 0.0
+		bottom = sum(OO)
+		for i in range(len(OO)):
+			top = top + (OO[i]-SO[i])
+		return (top/bottom)*100
+
+	def calculateSS(self, observedOutflow, simulatedOutflow):
+		OO = observedOutflow
+		SO = simulatedOutflow
 		top = 0.0
 		bottom = 0.0
+		meanOO = sum(OO)/len(OO)
 		for i in range(len(OO)):
-			top =top + (OO[i]-SO[i])**2
-			bottom = bottom + (abs(SO[i]-meanOO)+abs(OO[i]-meanOO))**2
-		return 1-top/bottom
+			top = top + (SO[i]-OO[i])**2
+			bottom = bottom + (meanOO - OO[i])**2
+		return 1 - top/bottom
+
+
+	
+
+	
 			
 
--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from StatisticalPerformanceMeasures import StatisticalPerformanceMeasures
 
 SPM = StatisticalPerformanceMeasures()
 option = 0
+OOI = SPM.getObservedOutflowIndividual()
+SOI = SPM.getSimulatedOutflowIndividual()
+OOA = SPM.getObservedOutflowAll()
+SOA = SPM.getSimulatedOutflowAll()
 while option == 0:
 	option = int(input("Please choose an option\n1 - Get individual sheet calculations\n2 - Get combined sheet calculations\n3 - Get both options\nOption:"))
 	if option <= 0 or option > 3:
@@ -12,24 +16,52 @@ while option == 0:
 if option == 1:
 	for i in range(SPM.getNumSheets()):
 		print("\nSheet", i+1)
-		print("NSE =", SPM.calculateNSE(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nPBIAS =", SPM.calculatePBIAS(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nRSR =", SPM.calculateRSR(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nd =", SPM.calculate_d(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]))
+		print("\nr =", SPM.calculate_r(OOI[i], SOI[i]),
+			"\nR^2 = ", SPM.calculateR2(OOI[i], SOI[i]),
+			"\nNSE =", SPM.calculateNSE(OOI[i], SOI[i]),
+			"\nd =", SPM.calculate_d(OOI[i], SOI[i]),
+			"\nRMSE = ", SPM.calculateRMSE(OOI[i], SOI[i]),
+			"\nMAE = ", SPM.calculateMAE(OOI[i], SOI[i]),
+			"\nRE = ", SPM.calculateRE(OOI[i], SOI[i]),
+			"\nRSR =", SPM.calculateRSR(OOI[i], SOI[i]),
+			"\nPBIAS =", SPM.calculatePBIAS(OOI[i], SOI[i]),
+			"\nSS = ", SPM.calculateSS(OOI[i], SOI[i]))
+		#if SPM.calculateSS(OOI[i], SOI[i]) == SPM.calculateNSE(OOI[i], SOI[i]):
+			#print("\nSS is the same as NSE")
+			
 elif option == 2:
-	print("NSE = ", SPM.calculateNSE(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nPBIAS = ", SPM.calculatePBIAS(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nRSR = ", SPM.calculateRSR(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nd = ", SPM.calculate_d(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()))
+	print("\nr =", SPM.calculate_r(OOA, SOA),
+			"\nR^2 = ", SPM.calculateR2(OOA, SOA),
+			"\nNSE =", SPM.calculateNSE(OOA, SOA),
+			"\nd =", SPM.calculate_d(OOA, SOA),
+			"\nRMSE = ", SPM.calculateRMSE(OOA, SOA),
+			"\nMAE = ", SPM.calculateMAE(OOA, SOA),
+			"\nRE = ", SPM.calculateRE(OOA, SOA),
+			"\nRSR =", SPM.calculateRSR(OOA, SOA),
+			"\nPBIAS =", SPM.calculatePBIAS(OOA, SOA),
+			"\nSS = ", SPM.calculateSS(OOA, SOA))
+
 elif option == 3:
 	for i in range(SPM.getNumSheets()):
 		print("\nSheet", i+1)
-		print("NSE =", SPM.calculateNSE(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nPBIAS =", SPM.calculatePBIAS(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nRSR =", SPM.calculateRSR(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]),
-			"\nd =", SPM.calculate_d(SPM.getObservedOutflowIndividual()[i], SPM.getSimulatedOutflowIndividual()[i]))
+		print("\nr =", SPM.calculate_r(OOI[i], SOI[i]),
+			"\nR^2 = ", SPM.calculateR2(OOI[i], SOI[i]),
+			"\nNSE =", SPM.calculateNSE(OOI[i], SOI[i]),
+			"\nd =", SPM.calculate_d(OOI[i], SOI[i]),
+			"\nRMSE = ", SPM.calculateRMSE(OOI[i], SOI[i]),
+			"\nMAE = ", SPM.calculateMAE(OOI[i], SOI[i]),
+			"\nRE = ", SPM.calculateRE(OOI[i], SOI[i]),
+			"\nRSR =", SPM.calculateRSR(OOI[i], SOI[i]),
+			"\nPBIAS =", SPM.calculatePBIAS(OOI[i], SOI[i]),
+			"\nSS = ", SPM.calculateSS(OOI[i], SOI[i]))
 	print("\nAll Sheets")
-	print("NSE = ", SPM.calculateNSE(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nPBIAS = ", SPM.calculatePBIAS(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nRSR = ", SPM.calculateRSR(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()),
-      "\nd = ", SPM.calculate_d(SPM.getObservedOutflowAll(), SPM.getSimulatedOutflowAll()))
+	print("\nr =", SPM.calculate_r(OOA, SOA),
+			"\nR^2 = ", SPM.calculateR2(OOA, SOA),
+			"\nNSE =", SPM.calculateNSE(OOA, SOA),
+			"\nd =", SPM.calculate_d(OOA, SOA),
+			"\nRMSE = ", SPM.calculateRMSE(OOA, SOA),
+			"\nMAE = ", SPM.calculateMAE(OOA, SOA),
+			"\nRE = ", SPM.calculateRE(OOA, SOA),
+			"\nRSR =", SPM.calculateRSR(OOA, SOA),
+			"\nPBIAS =", SPM.calculatePBIAS(OOA, SOA),
+			"\nSS = ", SPM.calculateSS(OOA, SOA))
